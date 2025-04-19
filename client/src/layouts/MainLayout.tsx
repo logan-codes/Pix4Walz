d027249c2536b07691ca525834e962e87f7a31a1
logan-codes <loganandamuthu2005@gmail.com>
2025-04-19 22:33:56 +0530

    feat: added Speed Insights

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const RootLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      <SpeedInsights/>
       <Navbar />
       <main className="flex-1">
         <Outlet />
