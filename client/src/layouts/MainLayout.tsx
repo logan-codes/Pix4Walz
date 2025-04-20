82fe0ff216abf0628f3926e63b99f854a2b96b46
logan-codes <loganandamuthu2005@gmail.com>
2025-04-20 16:34:42 +0530

    Revert "feat: added Analytics"
    This reverts commit 84a26f7044b5a1802738200890c2c02fe381a3db.

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -2,13 +2,11 @@ import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/react"
 
 const RootLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <SpeedInsights/>
-      <Analytics />
       <Navbar />
       <main className="flex-1">
         <Outlet />
